74548a84f6f2189b4ac5202ff415e44516e2f04e
adelina-chiriac <adelinachiriac@gmail.com>
2021-10-01 17:42:21 +0100

    Add event listener for create button submission

--- public/javascript/create.js
+++ public/javascript/create.js
@@ -1,1 +1,10 @@
-console.log("hello!!!!");
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    
+    const button = document.getElementById("create");
+    button.addEventListener("click", () => {
+        const days = document.getElementById("days").value;
+        console.log(days);
+    });
+});
+
