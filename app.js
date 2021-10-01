5e037f463f097ef8330a7207c6365eb3b70a083c
adelina-chiriac <adelinachiriac@gmail.com>
2021-10-01 13:50:26 +0100

    Serve index.html file for root route

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.listen(port, () => {
