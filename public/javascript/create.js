5b3f379a94a8db34d9a4af41c277da542e6bff45
adelina-chiriac <adelinachiriac@gmail.com>
2021-10-01 20:02:45 +0100

    Edit create files

--- public/javascript/create.js
+++ public/javascript/create.js
@@ -1,10 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    
-    const button = document.getElementById("create");
-    button.addEventListener("click", () => {
-        const days = document.getElementById("days").value;
-        console.log(days);
-    });
+
 });
 
