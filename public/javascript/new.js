5bc21e55e54084e3575f58caf44bb4282a24d016
adelina-chiriac <adelinachiriac@gmail.com>
2021-10-01 20:33:25 +0100

    Create an input & save button each time the add button is pressed

--- public/javascript/new.js
+++ public/javascript/new.js
@@ -1,8 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const buttons = document.querySelectorAll("button");
+    const divs = document.querySelectorAll("div");
     buttons.forEach((button, index) => {
         button.addEventListener("click", () => {
-            console.log(button, index);
+            // Create the input element
+            const input = document.createElement("input");
+            input.classList.add("link");
+            // Create the save button
+            const saveButton = document.createElement("button");
+            saveButton.classList.add("save");
+            saveButton.innerHTML = "Save";
+            // Add the input to the DOM
+            divs[index].insertBefore(input, button);
+            // Add the save button to the DOM
+            divs[index].insertBefore(saveButton, button);
         })
     })
     console.log(buttons);
